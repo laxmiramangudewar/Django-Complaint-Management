312ac86fed281797c8f168cd3d23c0a880968b89
Laxmiraman Dnyaneshwar Gudewar <132219054+laxmiramangudewar@users.noreply.github.com>
2025-06-14 00:19:56 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -13,6 +13,5 @@ if __name__ == "__main__":
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-    manage this 
-    manage that
+  
     
